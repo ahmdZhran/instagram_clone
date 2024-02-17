87172eb2bc5ce86ebc8ce19f2ab2f94afd1e46fc
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-17 10:18:47 +0200

    change string name

--- lib/features/auth/presentaion/views/sign_up.dart
+++ lib/features/auth/presentaion/views/sign_up.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/core/utls/app_colors.dart';
 import '../../../../core/widgets/custom_head_text_widget.dart';
 
 class SignUpView extends StatelessWidget {
@@ -14,10 +15,27 @@ class SignUpView extends StatelessWidget {
             children: [
               SizedBox(height: 150),
               CusotmHeadTextWidget(),
+              SizedBox(height: 50),
+              CustomTextForm()
             ],
           ),
         )
       ],
     ));
+  }
+}
+
+class CustomTextForm extends StatelessWidget {
+  const CustomTextForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(cursorColor: AppColors.kBlueColor),
+        ],
+      ),
+    );
   }
 }
