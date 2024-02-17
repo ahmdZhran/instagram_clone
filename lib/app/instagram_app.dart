cb0063512f76b30b684dd74bb11f585b9624dfe7
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-17 09:51:14 +0200

    create sign up view

--- lib/app/instagram_app.dart
+++ lib/app/instagram_app.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/core/utls/app_strings.dart';
+import 'package:instagram_clone/features/auth/presentaion/views/sign_up.dart';
 
 class InstagramApp extends StatelessWidget {
   const InstagramApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: AppStrings.appName,
       theme: ThemeData.dark(
         useMaterial3: true,
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SignUpView(),
     );
   }
 }
