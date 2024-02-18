a51b9a3f36eb1086cb71d21222909e48cfebfc68
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-18 07:52:52 +0200

    add Go router package & crete navigator methods

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:go_router/go_router.dart';
+import 'package:instagram_clone/features/auth/presentaion/views/sign_up.dart';
+
+import '../../features/auth/presentaion/views/sign_in.dart';
+
+final GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const SignUpView(),
+    ),
+    GoRoute(
+      path: '/SignUp',
+      builder: (context, state) => const SignInView(),
+    ),
+  ],
+);
