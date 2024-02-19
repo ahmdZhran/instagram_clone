778128b53006fb437294f3c13a4f17127341c31b
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-19 16:56:47 +0200

    create Auth state for authentacation

--- lib/features/auth/presentaion/view_model/auth_cubit.dart
+++ lib/features/auth/presentaion/view_model/auth_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,6 +13,26 @@ class AuthCubit extends Cubit<AuthState> {
   bool isObsecurePasswordText = true;
   GlobalKey<FormState> signUpFormKey = GlobalKey();
   GlobalKey<FormState> signInFormKey = GlobalKey();
+
+  createUserWithEmailAndPassword(
+      {required String emailAddress, required String password}) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   void obsecurePasswordText() {
     isObsecurePasswordText = !isObsecurePasswordText;
     emit(ObsecurePasswordTextState());
