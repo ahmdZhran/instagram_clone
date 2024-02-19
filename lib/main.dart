8b58da89072b6a01930dfae33bbfc9f1c830d397
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-19 16:48:25 +0200

    refactor our code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:instagram_clone/firebase_options.dart';
 import 'app/instagram_app.dart';
+import 'core/functions/current_user_stated.dart';
 
 void main() async {
   runApp(const InstagramApp());
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  currentUserStateCheck();
 }
