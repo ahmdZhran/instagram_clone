d9809b4dbb9b93ae09704b15f413453731c0a575
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-23 10:12:34 +0200

    refactor our code

---
+++ lib/features/auth/presentaion/widgets/forgot_password_form_body.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import '../../../../core/utls/app_assets.dart';
+import '../../../../core/utls/app_colors.dart';
+import '../../../../core/utls/app_strings.dart';
+import '../view_model/auth_cubit/auth_cubit.dart';
+import 'custom_button_widget.dart';
+import 'custom_text_form_field.dart';
+
+class ForgotPasswordFormBody extends StatelessWidget {
+  const ForgotPasswordFormBody({
+    super.key,
+    required this.authCubit,
+  });
+
+  final AuthCubit authCubit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: authCubit.resePassowrdKey,
+        child: Column(
+          children: [
+            CustomTextFormField(
+                onChanged: (emailAddress) {
+                  authCubit.emailAddress = emailAddress;
+                },
+                hintText: AppStrings.emailAddress),
+            const SizedBox(height: 50),
+            CustomButton(
+              onPressed: () {
+                if (authCubit.resePassowrdKey.currentState!.validate()) {
+                  authCubit.resetPasswordWithEmail();
+                }
+              },
+              color: AppColors.kBlueColor,
+              childOfCustomButton: authCubit.state is EmailResetPasswordLoading
+                  ? LottieBuilder.asset(Assets.lottieFilePath)
+                  : const Text(
+                      AppStrings.sendResetPasswordLink,
+                      style: TextStyle(
+                        color: AppColors.kWhiteColor,
+                      ),
+                    ),
+            ),
+          ],
+        ));
+  }
+}
--- lib/features/auth/presentaion/widgets/forgot_password_view.dart
+++ lib/features/auth/presentaion/widgets/forgot_password_view.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:instagram_clone/core/utls/app_colors.dart';
 import 'package:instagram_clone/core/utls/app_strings.dart';
 import 'package:instagram_clone/core/utls/text_styles.dart';
-import 'package:instagram_clone/features/auth/presentaion/widgets/custom_button_widget.dart';
 import 'package:instagram_clone/features/auth/presentaion/widgets/custom_snakbar_widget.dart';
-import 'package:instagram_clone/features/auth/presentaion/widgets/custom_text_form_field.dart';
-import 'package:lottie/lottie.dart';
 
-import '../../../../core/utls/app_assets.dart';
 import '../view_model/auth_cubit/auth_cubit.dart';
+import 'forgot_password_form_body.dart';
 
 class ForgotPasswordView extends StatelessWidget {
   const ForgotPasswordView({super.key});
@@ -45,36 +41,7 @@ class ForgotPasswordView extends StatelessWidget {
                     },
                     builder: (context, state) {
                       AuthCubit authCubit = BlocProvider.of<AuthCubit>(context);
-                      return Form(
-                          key: authCubit.resePassowrdKey,
-                          child: Column(
-                            children: [
-                              CustomTextFormField(
-                                  onChanged: (emailAddress) {
-                                    authCubit.emailAddress = emailAddress;
-                                  },
-                                  hintText: AppStrings.emailAddress),
-                              const SizedBox(height: 50),
-                              CustomButton(
-                                onPressed: () {
-                                  if (authCubit.resePassowrdKey.currentState!
-                                      .validate()) {
-                                    authCubit.resetPasswordWithEmail();
-                                  }
-                                },
-                                color: AppColors.kBlueColor,
-                                childOfCustomButton: state
-                                        is EmailResetPasswordLoading
-                                    ? LottieBuilder.asset(Assets.lottieFilePath)
-                                    : const Text(
-                                        AppStrings.sendResetPasswordLink,
-                                        style: TextStyle(
-                                          color: AppColors.kWhiteColor,
-                                        ),
-                                      ),
-                              ),
-                            ],
-                          ));
+                      return ForgotPasswordFormBody(authCubit: authCubit);
                     },
                   )
                 ],
