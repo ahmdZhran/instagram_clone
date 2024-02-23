982016e203c4ec74cc40e288eee445c27af08608
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-23 06:05:11 +0200

    add external_app_launcher package

--- lib/features/auth/presentaion/widgets/custom_snakbar_widget.dart
+++ lib/features/auth/presentaion/widgets/custom_snakbar_widget.dart
@@ -1,62 +1,4 @@
-// import 'package:flutter/material.dart';
-
-// void showSnakBarMessage(BuildContext context) {
-//   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//     content: Container(
-//       padding: const EdgeInsets.all(8),
-//       height: 70,
-//       decoration: const BoxDecoration(
-//         color: Colors.green,
-//         borderRadius: BorderRadius.all(
-//           Radius.circular(50),
-//         ),
-//       ),
-//       child: Row(children: [
-//         const Icon(
-//           Icons.check_circle,
-//           color: Colors.white,
-//           size: 40,
-//         ),
-//         const SizedBox(width: 20),
-//         Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             const Text(
-//               "Success",
-//               style: TextStyle(
-//                 fontSize: 18,
-//                 color: Colors.white,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             const Text(
-//               "Please check your email now to confirm.",
-//               style: TextStyle(color: Colors.white, fontSize: 12),
-//               maxLines: 2,
-//               overflow: TextOverflow.ellipsis,
-//             ),
-//             TextButton(
-//               onPressed: () async {
-//                 final url = Uri.parse('mailto:');
-//                 if (await launchUrl(url)) {
-//                   // handle successful launch
-//                 } else {
-//                   // handle launch failure
-//                 }
-//               },
-//               child: const Text('Open Email'),
-//             ),
-//           ],
-//         ),
-//       ]),
-//     ),
-//     backgroundColor: Colors.transparent,
-//     behavior: SnackBarBehavior.floating,
-//   ));
-// }
-
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void showSnakBarMessage(BuildContext context) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -69,46 +11,54 @@ void showSnakBarMessage(BuildContext context) {
           Radius.circular(50),
         ),
       ),
-      child: const Row(children: [
-        Icon(
-          Icons.check_circle,
-          color: Colors.white,
-          size: 40,
-        ),
-        SizedBox(width: 20),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Success",
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+      child: GestureDetector(
+        onTap: () {
+          print('tapped on Row=========================');
+        },
+        child: Row(children: [
+          const Icon(
+            Icons.check_circle,
+            color: Colors.white,
+            size: 40,
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "We've sent a verification email.",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Text(
-              "Please check your email now to confirm.",
-              style: TextStyle(color: Colors.white, fontSize: 12),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ],
-        ),
-      ]),
+              RichText(
+                text: const TextSpan(
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                  children: [
+                    TextSpan(
+                      text: "Press ",
+                    ),
+                    TextSpan(
+                      text: "here",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline),
+                    ),
+                    TextSpan(
+                      text: " to check your inbox.",
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ]),
+      ),
     ),
+    duration: const Duration(seconds: 5),
     backgroundColor: Colors.transparent,
     behavior: SnackBarBehavior.floating,
-    action: SnackBarAction(
-      label: 'Open Email',
-      onPressed: () async {
-        final url = Uri.parse('mailto:');
-        if (await launchUrl(url)) {
-          print('Email app launched successfully!');
-        } else {
-          print('Coudnt launch');
-        }
-      },
-    ),
   ));
 }
