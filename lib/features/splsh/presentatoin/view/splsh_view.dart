111387ef3ebd615ea9e28ffe87902fee50789248
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-25 08:28:00 +0200

    make splsh view initial view in app router

--- lib/features/splsh/presentatoin/view/splsh_view.dart
+++ lib/features/splsh/presentatoin/view/splsh_view.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
-class SplshView extends StatelessWidget {
+import '../../../../core/functions/navigator_methods.dart';
+import '../../../../core/utls/app_strings.dart';
+import '../../../../core/utls/text_styles.dart';
+
+class SplshView extends StatefulWidget {
   const SplshView({super.key});
 
   @override
+  State<SplshView> createState() => _SplshViewState();
+}
+
+class _SplshViewState extends State<SplshView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void delayedNavigation(context, path) {
+    Future.delayed(const Duration(seconds: 2), () {
+      customReplacementNavigate(context, path);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text(
+          AppStrings.appName,
+          style: CustomTextStyles.pacifico50Style,
+        ),
+      ),
+    );
   }
 }
