42b61004fa6667f1132b64842625f0ec67a84fd0
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-26 22:47:23 +0200

    toggle between views

--- lib/core/widgets/custom_bottom_navigation_bar.dart
+++ lib/core/widgets/custom_bottom_navigation_bar.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/core/widgets/bottom_nav_bar.dart';
+import 'package:instagram_clone/features/add_post/presentation/views/add_post.dart';
+import 'package:instagram_clone/features/home/presentation/views/home.dart';
+import 'package:instagram_clone/features/profile/presentatoin/views/profile.dart';
+import 'package:instagram_clone/features/search/presentation/views/search_view.dart';
+import 'package:instagram_clone/features/shorts/presentation/views/shorts.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
   const CustomBottomNavigationBar({Key? key}) : super(key: key);
@@ -26,11 +31,11 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
       body: IndexedStack(
         index: currentIndex,
         children: const [
-          Icon(Icons.home),
-          Icon(Icons.search),
-          Icon(Icons.add_a_photo),
-          Icon(Icons.add_a_photo),
-          Icon(Icons.person_outlined),
+          HomeView(),
+          SearchView(),
+          AddPost(),
+          ShortView(),
+          ProfileView()
         ],
       ),
     );
