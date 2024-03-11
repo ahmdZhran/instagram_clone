f064609e4af4c191e38e113841c833eb5691feb2
Ahmed <ahmedmohameddx80@gmail.com>
2024-03-11 14:22:49 +0200

    back to old code of profile avatar widget

--- lib/features/profile/presentatoin/widgets/profile_info_widgets/photo_of_profile.dart
+++ lib/features/profile/presentatoin/widgets/profile_info_widgets/photo_of_profile.dart
@@ -1,109 +1,26 @@
-// import 'package:flutter/material.dart';
-
-// class ProfileAvatarWidget extends StatelessWidget {
-//   const ProfileAvatarWidget({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(horizontal: 20),
-//       child: Container(
-//         height: 110,
-//         width: 110,
-//         decoration: const BoxDecoration(
-//           color: Colors.grey,
-//           shape: BoxShape.circle,
-//         ),
-//         child: ClipOval(
-//           child: Image.asset(
-//             'assets/images/airen.jpg',
-//             fit: BoxFit.cover,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
-class ProfileAvatarWidget extends StatefulWidget {
+class ProfileAvatarWidget extends StatelessWidget {
   const ProfileAvatarWidget({Key? key}) : super(key: key);
-
-  @override
-  _ProfileAvatarWidgetState createState() => _ProfileAvatarWidgetState();
-}
-
-class _ProfileAvatarWidgetState extends State<ProfileAvatarWidget> {
-  File? _image;
-
-  Future<void> _pickImage(ImageSource source) async {
-    final pickedFile = await ImagePicker().pickImage(source: source);
-
-    if (pickedFile != null) {
-      setState(() {
-        _image = File(pickedFile.path);
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: GestureDetector(
-        onTap: () {
-          _showImagePickerOptions();
-        },
-        child: Container(
-          height: 110,
-          width: 110,
-          decoration: const BoxDecoration(
-            color: Colors.grey,
-            shape: BoxShape.circle,
+      child: Container(
+        height: 110,
+        width: 110,
+        decoration: const BoxDecoration(
+          color: Colors.grey,
+          shape: BoxShape.circle,
+        ),
+        child: ClipOval(
+          child: Image.asset(
+            'assets/images/airen.jpg',
+            fit: BoxFit.cover,
           ),
-          child: _image != null
-              ? ClipOval(
-                  child: Image.file(
-                    _image!,
-                    fit: BoxFit.cover,
-                  ),
-                )
-              : const Icon(Icons.add_a_photo, size: 40),
         ),
       ),
-    );
-  }
-
-  void _showImagePickerOptions() {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return SafeArea(
-          child: Wrap(
-            children: [
-              ListTile(
-                leading: const Icon(Icons.photo_library),
-                title: const Text('Photo Library'),
-                onTap: () {
-                  _pickImage(ImageSource.gallery);
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.photo_camera),
-                title: const Text('Camera'),
-                onTap: () {
-                  _pickImage(ImageSource.camera);
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        );
-      },
     );
   }
 }
