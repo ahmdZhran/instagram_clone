ae5277fe1d9fb5875260e90ab5395c13e58672c3
Ahmed <ahmedmohameddx80@gmail.com>
2024-03-15 20:22:41 +0200

    add uploadProfileImage method

--- lib/features/profile/presentatoin/view_model/profile_cubit/user_repositry.dart
+++ lib/features/profile/presentatoin/view_model/profile_cubit/user_repositry.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../../../data/models/prfile_model.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class UserRepositry {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -16,5 +19,30 @@ class UserRepositry {
       bio: data['bio'],
       name: data['name'],
     );
+  }
+
+  Future<void> uploadProfileImage(String imagePath) async {
+    try {
+      final currentUser = FirebaseAuth.instance.currentUser;
+      if (currentUser == null) {
+        throw Exception('User not logged in');
+      }
+
+      final ref = firebase_storage.FirebaseStorage.instance
+          .ref()
+          .child('user_profile_images')
+          .child('${currentUser.uid}.jpg');
+
+      await ref.putFile(File(imagePath));
+
+      final imageUrl = await ref.getDownloadURL();
+
+      // Update user profile with image URL
+      await _firestore.collection("users").doc(currentUser.uid).update({
+        'profileImageUrl': imageUrl,
+      });
+    } catch (e) {
+      throw Exception('Failed to upload profile image: $e');
+    }
   }
 }
