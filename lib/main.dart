0049c8abb8ff58977bc9e67a54fe9b9551ea9d76
Ahmed <ahmedmohameddx80@gmail.com>
2024-03-16 06:17:43 +0200

    add androidProvider.playIntegrity

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +9,8 @@ import 'firebase_options.dart';
 void main() async {
   runApp(const InstagramApp());
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseAppCheck.instance
+      .activate(androidProvider: AndroidProvider.playIntegrity);
   currentUserStateCheck();
 }
