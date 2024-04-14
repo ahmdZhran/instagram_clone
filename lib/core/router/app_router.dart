cf13396476630710c71ae27c1b19fdf1aa3fab4c
Ahmed <ahmedmohameddx80@gmail.com>
2024-04-14 06:50:43 +0200

    create an instance from authrepoisty in router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/auth/data/repositry/firebase_auth_reposityr.dart';
 import '../../features/auth/presentaion/view_model/auth_cubit/auth_cubit.dart';
 import '../../features/auth/presentaion/views/sign_in_view.dart';
 import '../../features/auth/presentaion/views/sign_up_view.dart';
@@ -9,6 +10,8 @@ import '../../features/home/presentation/views/home.dart';
 import '../../features/splsh/presentatoin/view/splsh_view.dart';
 import '../widgets/custom_bottom_navigation_bar.dart';
 import 'route_constants.dart';
+
+final authRepository = FirebaseAuthRepository();
 
 final GoRouter router = GoRouter(
   routes: [
@@ -19,14 +22,14 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: RouteConstants.signUpRoute,
       builder: (context, state) => BlocProvider(
-        create: (context) => AuthCubit(),
+        create: (context) => AuthCubit(authRepository),
         child: const SignUpView(),
       ),
     ),
     GoRoute(
       path: RouteConstants.signInRoute,
       builder: (context, state) => BlocProvider(
-        create: (context) => AuthCubit(),
+        create: (context) => AuthCubit(authRepository),
         child: const SignInView(),
       ),
     ),
@@ -36,7 +39,7 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: RouteConstants.forgotPasswordRoute,
       builder: (context, state) => BlocProvider(
-        create: (context) => AuthCubit(),
+        create: (context) => AuthCubit(authRepository),
         child: const ForgotPasswordView(),
       ),
     ),
