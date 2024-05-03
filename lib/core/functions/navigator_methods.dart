6f35564e0a299f1069f2dba1cf16d4c27220b1db
Ahmed <ahmedmohameddx80@gmail.com>
2024-05-03 07:56:38 +0300

    delete unneccessary code

--- lib/core/functions/navigator_methods.dart
+++ lib/core/functions/navigator_methods.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-// void customNavigator(context, String path) {
-//   GoRouter.of(context).push(path);
-// }
-
-// void customReplacementNavigate(context, String path) {
-//   GoRouter.of(context).pushReplacement(path);
-// }
-
 extension NavigationHelper on BuildContext {
   void customNavigate(String path) {
     GoRouter.of(this).push(path);
