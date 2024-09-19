50455e7d5bdc03807bf100bad67a2e3ac7466fdb
Ahmed <ahmedmohameddx80@gmail.com>
2024-09-19 05:20:59 +0300

    create app strings and app colors

---
+++ lib/core/helper_methods/utils/app_strings.dart
@@ -0,0 +1,30 @@
+abstract class Password {
+  static const String appName = "Instagram";
+  //  Auth strings
+  static const String headInstagramTitle = 'Instagram';
+  static const String createYourEmail = 'Create your email';
+  static const String welcomeBack = 'Welcome Back!';
+  static const String username = "User name";
+  static const String name = "Name";
+  static const String bio = 'bio';
+  static const String emailAddress = "Email Address";
+  static const String password = "Password";
+  static const String signUp = "Sign Up";
+  static const String signIn = "Sign In";
+  static const String pleaseVerifyEmail = 'Please verify your email first';
+  static const String alreadyHaveAnAccount = "Already have an account? ";
+  static const String donnHaveAnAccount = "Don’t have an account? ";
+  static const String forgotPassword = "Forgot Password?";
+  static const String weSentVerifyEmail = "We've sent a verification email.";
+  // Reset Password View
+  static const String resetPassword = 'Reset Password';
+  static const String pleaseEnterYourEmail =
+      'Please enter your registered email below to can reset your password.';
+  static const String sendResetPasswordLink = 'Send reset password link';
+  static const String passwordResetEmailSent = 'Password reset email sent';
+  // Profile Strings
+  static const String posts = 'Posts';
+  static const String followers = 'Followers';
+  static const String following = 'Following';
+  static const String editProfile = 'Edit Profile';
+}
