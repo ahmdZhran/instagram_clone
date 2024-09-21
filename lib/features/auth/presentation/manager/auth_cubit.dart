09605407416d65830caf893ee6b4f8be887ae202
Ahmed <ahmedmohameddx80@gmail.com>
2024-09-22 02:04:25 +0300

    add methods to can provider or delete instance from it

--- lib/features/auth/presentation/manager/auth_cubit.dart
+++ lib/features/auth/presentation/manager/auth_cubit.dart
@@ -4,8 +4,25 @@ part 'auth_state.dart';
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
 
+  static const String _tag = "Cart_instance";
+  static AuthCubit getInstance(){
+    final isRegister = getIt.isRegistered<AuthCubit>(instanceName: _tag);
+    if(!isRegister){
+      getIt.registerSingleton<AuthCubit>(AuthCubit(),instanceName:_tag);
+      
+    }
+     return getIt.get<AuthCubit>(instanceName: _tag);
+  }
+  static Future<void> deleteInstance() async {
+    final isRegister = getIt.isRegistered<AuthCubit>(instanceName: _tag);
+    if (!isRegister) {
+      await getIt.unregister<AuthCubit>(instanceName: _tag);
+    }
+  }
+
+
   bool obscuredPasswordText = true;
-  
+
   void obscuredPassword() {
     obscuredPasswordText = !obscuredPasswordText;
     emit(ObscuredPasswordTextState());
