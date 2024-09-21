5b5b692249dae205cf3bc6c2c43c8211ac8e8be0
Ahmed <ahmedmohameddx80@gmail.com>
2024-09-22 01:45:49 +0300

    create first method in auth cubit - to can toggle between show or hide password

--- lib/features/auth/presentation/manager/auth_cubit.dart
+++ lib/features/auth/presentation/manager/auth_cubit.dart
@@ -3,4 +3,11 @@ part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
+
+  bool obscuredPasswordText = true;
+  
+  void obscuredPassword() {
+    obscuredPasswordText = !obscuredPasswordText;
+    emit(ObscuredPasswordTextState());
+  }
 }
