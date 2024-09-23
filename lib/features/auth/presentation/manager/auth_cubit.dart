dafdbd5518335999709ed544863e9ff87dc7cb19
Ahmed <ahmedmohameddx80@gmail.com>
2024-09-23 14:41:00 +0300

    correct name of string in auth cubit

--- lib/features/auth/presentation/manager/auth_cubit.dart
+++ lib/features/auth/presentation/manager/auth_cubit.dart
@@ -5,22 +5,21 @@ part 'auth_state.dart';
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
 
-  static const String _tag = "Cart_instance";
-  static AuthCubit getInstance(){
+  static const String _tag = "auth_instance";
+  static AuthCubit getInstance() {
     final isRegister = sl.isRegistered<AuthCubit>(instanceName: _tag);
-    if(!isRegister){
-      sl.registerSingleton<AuthCubit>(AuthCubit(),instanceName:_tag);
-      
+    if (!isRegister) {
+      sl.registerSingleton<AuthCubit>(AuthCubit(), instanceName: _tag);
     }
-     return sl.get<AuthCubit>(instanceName: _tag);
+    return sl.get<AuthCubit>(instanceName: _tag);
   }
+
   static Future<void> deleteInstance() async {
     final isRegister = sl.isRegistered<AuthCubit>(instanceName: _tag);
     if (!isRegister) {
       await sl.unregister<AuthCubit>(instanceName: _tag);
     }
   }
-
 
   bool obscuredPasswordText = true;
 
