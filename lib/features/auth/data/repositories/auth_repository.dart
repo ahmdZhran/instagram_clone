52a41f615bfef67bebc8b16f99637faca5ace27c
Ahmed <ahmedmohameddx80@gmail.com>
2024-09-24 20:16:43 +0300

    create sign up cubit and states - create DI and register classes there

---
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -0,0 +1,121 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+class AuthRepository {
+  final FirebaseAuth _auth;
+  final FirebaseFirestore _firestore;
+  final FirebaseStorage _firebaseStorage;
+
+  AuthRepository(
+      {required FirebaseAuth auth,
+      required FirebaseFirestore firestore,
+      required FirebaseStorage firebaseStorage})
+      : _auth = auth,
+        _firestore = firestore,
+        _firebaseStorage = firebaseStorage;
+  Future<User?> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String username,
+    required String name,
+    required String bio,
+    required Uint8List? profileImage,
+  }) async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      String imageUrl =
+          await _uploadProfileImage(profileImage!, userCredential.user!.uid);
+
+      await _firestore.collection('users').doc(userCredential.user!.uid).set({
+        "username": username,
+        "name": name,
+        "bio": bio,
+        "profileImage": imageUrl,
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+    } on FirebaseAuthException catch (e) {
+      String errorMessage;
+      switch (e.code) {
+        case 'weak-password':
+          errorMessage = 'The password provided is too weak.';
+          break;
+        case 'email-already-in-use':
+          errorMessage = 'The account already exists for that email.';
+          break;
+        case 'invalid-email':
+          errorMessage = 'The email is invalid';
+          break;
+        default:
+          errorMessage = e.message ?? 'An error occurred';
+      }
+      throw Exception(errorMessage);
+    }
+    return null;
+  }
+
+  Future<String> _uploadProfileImage(Uint8List image, String userId) async {
+    try {
+      TaskSnapshot snapshot = await _firebaseStorage
+          .ref()
+          .child('profileImages/$userId')
+          .putData(image);
+      return await snapshot.ref.getDownloadURL();
+    } catch (error) {
+      debugPrint('error to upload image profile$error');
+      rethrow;
+    }
+  }
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      String errorMessage;
+      switch (e.code) {
+        case 'user-not-found':
+          errorMessage = 'No user found for that email.';
+          break;
+        case 'wrong-password':
+          errorMessage = 'Wrong password provided for that user.';
+          break;
+        default:
+          errorMessage = 'An error occurred during sign in.';
+      }
+      throw Exception(errorMessage);
+    }
+  }
+
+  Future<void> resetPasswordWithEmail({
+    required String email,
+  }) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      String errorMessage;
+      if (e.code == 'invalid-email') {
+        errorMessage = 'Please enter a valid email.';
+      } else {
+        errorMessage = 'An error occurred while resetting password.';
+      }
+      throw Exception(errorMessage);
+    }
+  }
+
+  Future<User?> getCurrentUser() async {
+    return _auth.currentUser;
+  }
+}
--- lib/features/auth/presentation/manager/auth_cubit.dart
+++ lib/features/auth/presentation/manager/auth_cubit.dart
@@ -1,19 +1,79 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:instagram_clone/core/utils/injection_container.dart';
+import 'package:instagram_clone/features/auth/auth_di.dart';
+
+import '../../../../core/helper/image_picker_service.dart';
+import '../../data/repositories/auth_repository.dart';
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(AuthInitial());
+  AuthCubit(this._authRepository, this._imagePickerService)
+      : super(AuthInitial());
 
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailAddressController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController bioController = TextEditingController();
+  Uint8List? profileImage;
+
+  final AuthRepository _authRepository;
+  final ImagePickerService _imagePickerService;
   GlobalKey<FormState> loginFormKey = GlobalKey();
   GlobalKey<FormState> signUpKey = GlobalKey();
 
+  bool obscuredPasswordText = true;
+
+  Future<void> createUserWithEmailAndPassword() async {
+    emit(CreateUserLoading());
+
+    try {
+      await _authRepository.createUserWithEmailAndPassword(
+        email: emailAddressController.text,
+        password: passwordController.text,
+        username: usernameController.text,
+        name: nameController.text,
+        bio: bioController.text,
+        profileImage: profileImage,
+      );
+
+      emit(CreateUserSuccess());
+    } catch (e) {
+      emit(CreateUserFailure(errMessage: e.toString()));
+    }
+  }
+
+  Future<void> selectedImageProfile() async {
+    Uint8List? image = await _imagePickerService.pickImage(ImageSource.gallery);
+    profileImage = image;
+    emit(ProfileImageSelected(profileImage));
+  }
+
+  void obscuredPassword() {
+    obscuredPasswordText = !obscuredPasswordText;
+    emit(ObscuredPasswordTextState());
+  }
+
+  //TODO check user auth state
+//  Future<void> checkAuthStatus() async {
+//       final user = await _authRepository.getCurrentUser();
+//       if (user != null) {
+//         emit(AuthSuccess(user));
+//       } else {
+//         emit(AuthLoggedOut());
+//       }
+//     }
   static const String _tag = "auth_instance";
   static AuthCubit getInstance() {
     final isRegister = sl.isRegistered<AuthCubit>(instanceName: _tag);
     if (!isRegister) {
-      sl.registerSingleton<AuthCubit>(AuthCubit(), instanceName: _tag);
+      sl.registerSingleton<AuthCubit>(
+          AuthCubit(authDI(), sl<ImagePickerService>()),
+          instanceName: _tag);
     }
     return sl.get<AuthCubit>(instanceName: _tag);
   }
@@ -23,12 +83,5 @@ class AuthCubit extends Cubit<AuthState> {
     if (isRegister) {
       await sl.unregister<AuthCubit>(instanceName: _tag);
     }
-  }
-
-  bool obscuredPasswordText = true;
-
-  void obscuredPassword() {
-    obscuredPasswordText = !obscuredPasswordText;
-    emit(ObscuredPasswordTextState());
   }
 }
