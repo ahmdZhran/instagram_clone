1a06be06a18a6b0da99c3e5e5e76e260d6ce92c4
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-06 09:47:50 +0300

    change padding and margin value

--- lib/core/utils/app_responsive_spacer_helper.dart
+++ lib/core/utils/app_responsive_spacer_helper.dart
@@ -6,7 +6,7 @@ class AppResponsiveSpacerHelper {
   static final EdgeInsets mediumPadding = EdgeInsets.all(16.w);
   static final EdgeInsets largePadding = EdgeInsets.all(24.w);
 
-  static final EdgeInsets mediumMargin = EdgeInsets.symmetric(horizontal: 20.w);
+  static final EdgeInsets mediumMargin = EdgeInsets.symmetric(horizontal: 16.w);
 
   static final EdgeInsets horizontalPadding =
       EdgeInsets.symmetric(horizontal: 20.w);
@@ -15,6 +15,6 @@ class AppResponsiveSpacerHelper {
 
   static final EdgeInsets customPadding = EdgeInsets.symmetric(
     horizontal: 30.w,
-    vertical: 18.h,
+    vertical: 14.h,
   );
 }
