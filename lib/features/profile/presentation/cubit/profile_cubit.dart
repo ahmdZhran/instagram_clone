a0df8552a1121aef3c88bde3f7ebded6a224910e
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-10 08:27:15 +0300

    create toggleTheme method to can change our mode and save it in shared preferences - create load theme to can get the final theme that we selected

--- lib/features/profile/presentation/cubit/profile_cubit.dart
+++ lib/features/profile/presentation/cubit/profile_cubit.dart
@@ -10,9 +10,22 @@ class ProfileCubit extends Cubit<ProfileState> {
   static const _themeKey = "theme";
   final SharedPrefHelper sharedPrefHelper;
 
-  void toggleTheme(bool isDarkMode)async{
-    if(isDarkMode){
+  void toggleTheme(bool isDarkMode) async {
+    if (isDarkMode) {
+      emit(ProfileThemeChanged(themeData: ThemeData.dark()));
+      await sharedPrefHelper.saveData(key: _themeKey, value: true);
+    } else {
+      emit(ProfileThemeChanged(themeData: ThemeData.light()));
+      await sharedPrefHelper.saveData(key: _themeKey, value: false);
+    }
+  }
 
+  void loadTheme() {
+    final bool isDarkMode = SharedPrefHelper().getData(key: _themeKey) ?? false;
+    if (isDarkMode) {
+      emit(ProfileThemeChanged(themeData: ThemeData.dark()));
+    } else {
+      emit(ProfileThemeChanged(themeData: ThemeData.light()));
     }
   }
 }
