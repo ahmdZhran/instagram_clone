c6096a7ffbe2b7a944e1fcf8fd713887d257d9f0
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-10 19:11:24 +0300

    initial our profile DI for dependency injection

--- lib/core/utils/injection_container.dart
+++ lib/core/utils/injection_container.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:instagram_clone/features/profile/profile_di.dart';
 
 import '../../features/auth/auth_di.dart';
 
@@ -7,5 +8,6 @@ final sl = GetIt.instance;
 class InjectionContainer {
   Future<void> init() async {
     AuthDI().init();
+    ProfileDI().init();
   }
 }
---
+++ lib/features/profile/profile_di.dart
@@ -0,0 +1,8 @@
+import 'package:get_it/get_it.dart';
+
+final profileDI = GetIt.instance;
+class ProfileDI {
+  Future<void> init()async{
+    
+  }
+}
