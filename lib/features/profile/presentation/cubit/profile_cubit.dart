80091d5e674ef4e3fd62a05160821975f47de008
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-10 08:21:53 +0300

    create profile changed theme data

--- lib/features/profile/presentation/cubit/profile_cubit.dart
+++ lib/features/profile/presentation/cubit/profile_cubit.dart
@@ -1,7 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instagram_clone/core/helper/shared_pref_helper.dart';
 
 part 'profile_state.dart';
 
 class ProfileCubit extends Cubit<ProfileState> {
-  ProfileCubit() : super(ProfileInitial());
+  ProfileCubit(this.sharedPrefHelper) : super(ProfileInitial());
+
+  static const _themeKey = "theme";
+  final SharedPrefHelper sharedPrefHelper;
+
+  void toggleTheme(bool isDarkMode)async{
+    if(isDarkMode){
+
+    }
+  }
 }
--- lib/features/profile/presentation/cubit/profile_state.dart
+++ lib/features/profile/presentation/cubit/profile_state.dart
@@ -3,3 +3,10 @@ part of 'profile_cubit.dart';
 sealed class ProfileState {}
 
 final class ProfileInitial extends ProfileState {}
+
+class ProfileThemeChanged extends ProfileState {
+  final ThemeData themeData;
+
+  ProfileThemeChanged({required this.themeData});
+
+}
