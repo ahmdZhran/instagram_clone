b053ef4ac643317803ede8e23f44ad03e82330c0
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-12 14:37:56 +0300

    change class name to don't be specific on only navigation extension

--- lib/core/helper/extensions.dart
+++ lib/core/helper/extensions.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-extension Navigation on BuildContext {
+extension ExtensionsHelper on BuildContext {
+
+  
+
   Future<dynamic> pushNamed(String routeName, {Object? arguments}) {
     return Navigator.of(this).pushNamed(routeName, arguments: arguments);
   }
