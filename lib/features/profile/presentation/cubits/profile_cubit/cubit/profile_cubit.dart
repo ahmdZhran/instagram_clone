08d7920aa54e1647093d86d34ee2a097817fa9e6
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-20 09:37:50 +0300

    create all state in profile state that we will need

--- lib/features/profile/presentation/cubits/profile_cubit/cubit/profile_cubit.dart
+++ lib/features/profile/presentation/cubits/profile_cubit/cubit/profile_cubit.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instagram_clone/features/auth/domain/entities/user_data_entity.dart';
 
 part 'profile_state.dart';
 
--- lib/features/profile/presentation/cubits/profile_cubit/cubit/profile_state.dart
+++ lib/features/profile/presentation/cubits/profile_cubit/cubit/profile_state.dart
@@ -3,3 +3,17 @@ part of 'profile_cubit.dart';
 sealed class ProfileState {}
 
 final class ProfileInitial extends ProfileState {}
+
+final class ProfileLoading extends ProfileState {}
+
+final class ProfileSuccess extends ProfileState {
+  final UserDataEntity userData;
+
+  ProfileSuccess({required this.userData});
+}
+
+final class ProfileFailure extends ProfileState {
+  final String errMessage;
+
+  ProfileFailure({required this.errMessage});
+}
