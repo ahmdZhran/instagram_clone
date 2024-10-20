cbb6a417db75f6cb236939c24473067dd231aeb9
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-20 07:49:37 +0300

    create user entity model

---
+++ lib/features/profile/domain/entities/user_profile_entity.dart
@@ -0,0 +1,13 @@
+class UserEntity {
+  final String username;
+  final String name;
+  final String bio;
+  final String profileImageUrl;
+
+  UserEntity({
+    required this.username,
+    required this.name,
+    required this.bio,
+    required this.profileImageUrl,
+  });
+}
