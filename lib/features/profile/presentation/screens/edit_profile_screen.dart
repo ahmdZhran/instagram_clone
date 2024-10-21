decd9729ee912e9e095cac75aac78c3cf08c1607
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-21 23:04:10 +0300

    create app bar in edit profile screen

--- lib/features/profile/presentation/screens/edit_profile_screen.dart
+++ lib/features/profile/presentation/screens/edit_profile_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/core/helper/extensions.dart';
+import 'package:instagram_clone/core/utils/app_strings.dart';
 
 class EditProfileScreen extends StatelessWidget {
   const EditProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back),
+        title: Text(
+          context.translate(AppStrings.editProfile),
+        ),
+      ),
+    );
   }
 }
