f2d413a9a34aa6eb40e46d99fea595fe278f712b
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-26 23:45:38 +0300

    make _saveProfileChanges method future method

--- lib/features/profile/presentation/screens/edit_profile_screen.dart
+++ lib/features/profile/presentation/screens/edit_profile_screen.dart
@@ -64,7 +64,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
             ScaffoldMessenger.of(context).showSnackBar(
               const SnackBar(content: Text("Profile updated successfully")),
             );
-            Navigator.pop(context); 
+            Navigator.pop(context);
           } else if (state is ProfileUpdateFailure) {
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(content: Text(state.errMessage)),
@@ -147,7 +147,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
         (profileImage?.isNotEmpty ?? false);
   }
 
-  void _saveProfileChanges() async {
+  Future<void> _saveProfileChanges() async {
     try {
       String? profileImageUrl = widget.userProfileData.profileImageUrl;
 
