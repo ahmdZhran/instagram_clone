163a51488732de8d48dc60947698b8b2645d8019
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-28 23:33:10 +0300

    add iconsax icon package

--- lib/features/add_media/presentation/screens/add_post_screen.dart
+++ lib/features/add_media/presentation/screens/add_post_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import '../widgets/instagram_media_picker.dart';
 
 class AddPostScreen extends StatelessWidget {
   const AddPostScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: InstagramMediaPicker(),
+    );
   }
 }
