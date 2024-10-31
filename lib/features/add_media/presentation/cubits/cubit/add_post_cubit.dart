3d2fef64dccb4032b4558a15f03efb7dd1d06382
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-31 16:36:59 +0300

    create add post states

--- lib/features/add_media/presentation/cubits/cubit/add_post_cubit.dart
+++ lib/features/add_media/presentation/cubits/cubit/add_post_cubit.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instagram_clone/features/add_media/data/models/media_model.dart';
+import 'package:photo_manager/photo_manager.dart';
 part 'add_post_state.dart';
 
 class AddPostCubit extends Cubit<AddPostState> {
--- lib/features/add_media/presentation/cubits/cubit/add_post_state.dart
+++ lib/features/add_media/presentation/cubits/cubit/add_post_state.dart
@@ -3,3 +3,16 @@ part of 'add_post_cubit.dart';
 sealed class AddPostState {}
 
 final class AddPostInitial extends AddPostState {}
+
+final class AlbumsLoaded extends AddPostState {
+  final List<AssetPathEntity> albums;
+  final AssetPathEntity currentAlbums;
+
+  AlbumsLoaded(this.albums, this.currentAlbums);
+}
+
+final class MediaLoaded extends AddPostState {
+  final List<MediaModel> medias;
+
+  MediaLoaded(this.medias);
+}
