9da2e2d1b7db1d8c14342ff548d3dc174767b68f
Ahmed <ahmedmohameddx80@gmail.com>
2024-10-31 17:34:52 +0300

    create addPostCubit methods - load albums - load more images from your current album - change album

--- lib/features/add_media/presentation/cubits/cubit/add_post_cubit.dart
+++ lib/features/add_media/presentation/cubits/cubit/add_post_cubit.dart
@@ -1,9 +1,67 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:instagram_clone/features/add_media/data/models/media_model.dart';
 import 'package:photo_manager/photo_manager.dart';
+
+import '../../../../../core/services/media_services.dart';
 part 'add_post_state.dart';
 
 class AddPostCubit extends Cubit<AddPostState> {
-  AddPostCubit() : super(AddPostInitial());
-  
+  AddPostCubit({required this.mediaService}) : super(AddPostInitial());
+  final MediaService mediaService;
+
+  Future<void> loadAlbums() async {
+    emit(MediaLoading());
+    try {
+      final albums = await mediaService.fetchAlbums();
+      if (albums.isNotEmpty) {
+        final currentAlbums = albums.first;
+        final medias =
+            await mediaService.fetchMedias(album: currentAlbums, page: 0);
+        emit(
+          MediaLoaded(
+              currentAlbum: currentAlbums, medias: medias, albums: albums),
+        );
+      } else {
+        emit(MediaError(error: "We can't find albums"));
+      }
+    } catch (error) {
+      emit(MediaError(error: "Something went wrong during get albums"));
+    }
+  }
+
+  Future<void> loadMoreMediasInAlbum(AssetPathEntity album, int page) async {
+    if (state is MediaLoaded) {
+      final currentState = state as MediaLoaded;
+
+      try {
+        final newMedias =
+            await mediaService.fetchMedias(album: album, page: page);
+        emit(MediaLoaded(
+          albums: currentState.albums,
+          medias: List.from(currentState.medias)..addAll(newMedias),
+          currentAlbum: currentState.currentAlbum,
+        ));
+      } catch (e) {
+        emit(MediaError(error: "Something went wrong"));
+      }
+    }
+  }
+
+  Future<void> changeAlbum(AssetPathEntity album) async {
+    emit(MediaLoading());
+    try {
+      final medias = await mediaService.fetchMedias(album: album, page: 0);
+      final currentState = state as MediaLoaded;
+
+      emit(
+        MediaLoaded(
+          albums: currentState.albums,
+          currentAlbum: album,
+          medias: medias,
+        ),
+      );
+    } catch (e) {
+      emit(MediaError(error: "something went wrong"));
+    }
+  }
 }
--- lib/features/add_media/presentation/cubits/cubit/add_post_state.dart
+++ lib/features/add_media/presentation/cubits/cubit/add_post_state.dart
@@ -4,15 +4,19 @@ sealed class AddPostState {}
 
 final class AddPostInitial extends AddPostState {}
 
-final class AlbumsLoaded extends AddPostState {
-  final List<AssetPathEntity> albums;
-  final AssetPathEntity currentAlbums;
-
-  AlbumsLoaded(this.albums, this.currentAlbums);
-}
+final class MediaLoading extends AddPostState {}
 
 final class MediaLoaded extends AddPostState {
   final List<MediaModel> medias;
+  final List<AssetPathEntity> albums;
+  final AssetPathEntity currentAlbum;
 
-  MediaLoaded(this.medias);
+  MediaLoaded(
+      {required this.albums, required this.currentAlbum, required this.medias});
+}
+
+class MediaError extends AddPostState {
+  final String error;
+
+  MediaError({required this.error});
 }
