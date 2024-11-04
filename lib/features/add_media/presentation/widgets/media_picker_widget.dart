36a43d26646e91eedcefa76cd562e7b261de55b4
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-04 08:27:17 +0200

    "Removed imports and refactored PickImagePostWidget to use MediaPickerWidget"

---
+++ lib/features/add_media/presentation/widgets/media_picker_widget.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:instagram_clone/core/helper/extensions.dart';
+import 'package:instagram_clone/core/services/media_services.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+import '../../../../core/utils/custom_text_style.dart';
+
+class MediaPickerWidget extends StatefulWidget {
+  const MediaPickerWidget(
+      {super.key, required this.maxCount, required this.requestType});
+  final int maxCount;
+  final RequestType requestType;
+  @override
+  State<MediaPickerWidget> createState() => _MediaPickerWidgetState();
+}
+
+class _MediaPickerWidgetState extends State<MediaPickerWidget> {
+  AssetPathEntity? selectedAlbum;
+  List<AssetPathEntity> albumList = [];
+  List<AssetEntity> assetList = [];
+  List<AssetEntity> selectedAssetsList = [];
+  @override
+  void initState() {
+    MediaServices().loadAlbums(widget.requestType).then((value) {
+      setState(() {
+        albumList = value;
+        selectedAlbum = value[0];
+      });
+      MediaServices().loadAssets(selectedAlbum!).then((value) {
+        setState(() {
+          assetList = value;
+        });
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40.h,
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: [
+            Text(selectedAlbum?.name ?? "Select Album",
+                style: CustomTextStyle.pacifico20),
+            IconButton(
+              icon: const Icon(Iconsax.arrow_down_1),
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return ListView.builder(
+                      itemCount: albumList.length,
+                      itemBuilder: (context, index) {
+                        final album = albumList[index];
+                        return ListTile(
+                          title: Text(album.name),
+                          onTap: () {
+                            setState(() {
+                              selectedAlbum = album;
+                            });
+                            context.pop();
+                          },
+                        );
+                      },
+                    );
+                  },
+                );
+              },
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.multiple_stop),
+            ),
+            IconButton(
+              enableFeedback: true,
+              icon: const Icon(Icons.camera),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/add_media/presentation/widgets/pick_image_post_widget.dart
+++ lib/features/add_media/presentation/widgets/pick_image_post_widget.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:gap/gap.dart';
-import 'package:iconsax/iconsax.dart';
+import 'package:photo_manager/photo_manager.dart';
 
-import 'package:instagram_clone/core/utils/custom_text_style.dart';
+import 'package:instagram_clone/features/add_media/presentation/widgets/media_picker_widget.dart';
 import 'package:instagram_clone/features/add_media/presentation/widgets/picked_image_interaction_widget.dart';
 
 class PickImagePostWidget extends StatefulWidget {
@@ -16,33 +14,13 @@ class PickImagePostWidget extends StatefulWidget {
 class PickImagePostWidgetState extends State<PickImagePostWidget> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Column(
         children: [
-          const PickerImageInteractionWidget(),
-          SizedBox(
-            height: 40.h,
-            width: double.infinity,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Row(
-                children: [
-                  Text('Recent', style: CustomTextStyle.pacifico20),
-                  const Gap(10),
-                  const Icon(Iconsax.arrow_down_1),
-                  const Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.multiple_stop),
-                  ),
-                  IconButton(
-                    enableFeedback: true,
-                    icon: const Icon(Icons.camera),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ),
+          PickerImageInteractionWidget(),
+          MediaPickerWidget(
+            maxCount: 5,
+            requestType: RequestType.image,
           ),
         ],
       ),
