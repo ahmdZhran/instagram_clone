fa30c644e2c550f12abe2ff028027731e17222e6
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-10 14:42:38 +0200

    "Added FirebaseStorageService to AddPostDi and removed empty lines"

--- lib/features/add_post/add_post_di.dart
+++ lib/features/add_post/add_post_di.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
+import 'package:instagram_clone/core/services/firebase_storage_service.dart';
 import 'package:instagram_clone/features/add_post/data/repositories/add_post_repository_impl.dart';
 import 'package:instagram_clone/features/add_post/domain/repositories/add_post_repository.dart';
 
@@ -8,10 +9,8 @@ final addPostDi = GetIt.instance;
 class AddPostDi {
   Future<void> init() async {
     addPostDi.registerLazySingleton(() => FirebaseFirestore.instance);
+    addPostDi.registerSingleton(() => FirebaseStorageService());
     addPostDi.registerLazySingleton<AddPostRepository>(
         () => AddPostRepositoryImpl());
   }
-
-
-   
 }
