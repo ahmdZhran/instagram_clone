fc69ad1687c8b24ead3c2aa251382ec53974216e
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-20 14:06:21 +0200

    "Removed unnecessary imports and refactored PostsSectionWidget to use PostsListWidget"

---
+++ lib/features/home/presentation/widgets/posts_list_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../../posts/domain/entities/post_entity.dart';
+import 'post_card_widget.dart';
+
+class PostsList extends StatelessWidget {
+  final List<PostEntity> posts;
+
+  const PostsList({required this.posts, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      shrinkWrap: true,
+      scrollDirection: Axis.vertical,
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        final post = posts[index];
+        return PostCard(post: post);
+      },
+    );
+  }
+}
+
--- lib/features/home/presentation/widgets/posts_section_widget.dart
+++ lib/features/home/presentation/widgets/posts_section_widget.dart
@@ -1,20 +1,8 @@
-import 'dart:ui';
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:gap/gap.dart';
-import 'package:insta_image_viewer/insta_image_viewer.dart';
-import 'package:instagram_clone/features/posts/presentation/cubit/posts_cubit.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
-import '../../../../core/utils/app_colors.dart';
-import '../../../../core/utils/custom_chached_network_image.dart';
-import '../../../../core/utils/custom_short_messages_for_time_ago.dart';
-import '../../../../core/utils/custom_text_style.dart';
-import 'circle_profile_image_widget.dart';
+import '../../../../features/posts/presentation/cubit/posts_cubit.dart';
 import 'post_loading_shimmer_widget.dart';
-import 'react_icons_widget.dart';
-import 'package:timeago/timeago.dart' as timeago;
+import 'posts_list_widget.dart';
 
 class PostsSectionWidget extends StatefulWidget {
   const PostsSectionWidget({super.key});
@@ -25,10 +13,11 @@ class PostsSectionWidget extends StatefulWidget {
 
 class _PostsSectionWidgetState extends State<PostsSectionWidget> {
   final PostsCubit _postsCubit = PostsCubit.getInstance();
+
   @override
   void initState() {
-    _postsCubit.fetchPosts();
     super.initState();
+    _postsCubit.fetchPosts();
   }
 
   @override
@@ -40,112 +29,9 @@ class _PostsSectionWidgetState extends State<PostsSectionWidget> {
           if (state is PostsLoading) {
             return const PostsLoadingShimmerWidget();
           } else if (state is PostsFailure) {
-            return const Center(
-              child: Text('Failed to load posts'),
-            );
+            return const Center(child: Text('Failed to load posts'));
           } else if (state is PostsSuccess) {
-            final posts = state.posts;
-            return ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              itemCount: posts!.length,
-              itemBuilder: (context, index) {
-                final post = posts[index];
-                timeago.setLocaleMessages('custom', CustomShortMessagesForTimeAgo());
-                final timeAgo =
-                    timeago.format(post.timestamp, locale: 'custom');
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 20, left: 5, right: 5),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(25.0),
-                    child: Stack(
-                      children: [
-                        Container(
-                          width: double.infinity,
-                          height: 420.0.h,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25.0),
-                            gradient: LinearGradient(
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                              colors: [
-                                AppColors.greyColor.withOpacity(0.5),
-                                AppColors.greyColor.withOpacity(0.3),
-                              ],
-                            ),
-                          ),
-                        ),
-                        BackdropFilter(
-                          filter: ImageFilter.blur(
-                            sigmaX: 5.0,
-                            sigmaY: 5.0,
-                          ),
-                          child: Column(
-                            children: [
-                              ListTile(
-                                contentPadding: const EdgeInsets.symmetric(
-                                  horizontal: 10,
-                                  vertical: 10,
-                                ),
-                                horizontalTitleGap: 10,
-                                dense: true,
-                                leading: CircleProfileImageWidget(
-                                    userProfileImage: post.userProfileImage,),
-                                title: Text(post.username,
-                                    style: CustomTextStyle.pacifico13),
-                                subtitle: Text(timeAgo),
-                                trailing: const Icon(Icons.more_horiz),
-                              ),
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(20.0),
-                                child: SizedBox(
-                                  width: 300.w,
-                                  height: 300.h,
-                                  child: InstaImageViewer(
-                                    imageUrl: post.imageUrl,
-                                    child: CustomCachedNetworkImage(
-                                      imageUrl: post.imageUrl,
-                                      fit: BoxFit.cover,
-                                      placeholder: Center(
-                                        child: LoadingAnimationWidget
-                                            .threeRotatingDots(
-                                                color: AppColors.primaryColor,
-                                                size: 40),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              const Gap(10),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 20,
-                                ),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Flexible(
-                                      child: AutoSizeText(
-                                        overflow: TextOverflow.ellipsis,
-                                        post.description.toString(),
-                                        maxLines: 1,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const Gap(10),
-                              const ReactIconsWidget()
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            );
+            return PostsList(posts: state.posts ?? []);
           }
           return const SizedBox.shrink();
         },
@@ -153,3 +39,11 @@ class _PostsSectionWidgetState extends State<PostsSectionWidget> {
     );
   }
 }
+
+
+
+
+
+
+
+
