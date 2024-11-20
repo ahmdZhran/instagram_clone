606f0c6959e1aa487bf9af71de6ff2c4e11e49cb
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-20 12:55:27 +0200

    Remove unnecessary import of 'skeletonizer' package in CircleProfileImageWidget.

--- lib/features/home/presentation/widgets/circle_profile_image_widget.dart
+++ lib/features/home/presentation/widgets/circle_profile_image_widget.dart
@@ -1,7 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:skeletonizer/skeletonizer.dart';
-
 import '../../../../core/utils/app_colors.dart';
 
 class CircleProfileImageWidget extends StatelessWidget {
