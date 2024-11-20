86e02f95bc8cd97705905169b93d62060ef38613
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-20 09:45:11 +0200

    "Rename AddPostRepositoryImpl to PostRepositoryImpl and update import in add_post_di.dart; delete AddPostRepositoryImpl file"

--- lib/features/posts/add_post_di.dart
+++ lib/features/posts/add_post_di.dart
@@ -1,5 +1,5 @@
 import 'package:get_it/get_it.dart';
-import 'package:instagram_clone/features/posts/data/repositories/add_post_repository_impl.dart';
+import 'package:instagram_clone/features/posts/data/repositories/post_repository_impl.dart';
 import 'package:instagram_clone/features/posts/domain/repositories/add_post_repository.dart';
 
 final addPostDi = GetIt.instance;
