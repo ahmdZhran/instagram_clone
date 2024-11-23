693ecf19904b183624eaf56332f2c9f04bf91369
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-23 16:10:36 +0200

    Remove import of 'package:path_provider/path_provider.dart' from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:path_provider/path_provider.dart';
 import 'core/router/app_router.dart';
 import 'my_bloc_observer.dart';
 import 'app/instagram_app.dart';
