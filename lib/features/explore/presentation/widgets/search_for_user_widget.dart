fe778f7a709415004096b02fd345479e8406a160
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-24 11:16:34 +0200

    "Removed unnecessary import, rearranged import order in SearchForUserWidget"

--- lib/features/explore/presentation/widgets/search_for_user_widget.dart
+++ lib/features/explore/presentation/widgets/search_for_user_widget.dart
@@ -1,12 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:instagram_clone/core/helper/extensions.dart';
-import 'package:instagram_clone/features/explore/presentation/cubit/explore_cubit.dart';
-
+import '../../../../core/helper/extensions.dart';
+import '../cubit/explore_cubit.dart';
 import '../../../../core/utils/app_colors.dart';
 import '../../../../core/widgets/custom_text_form_field.dart';
 
