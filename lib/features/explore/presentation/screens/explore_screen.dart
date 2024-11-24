2ef20de7a13dbf798419d0d57ef1bab2da674864
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-24 07:38:14 +0200

    remove unused import files

--- lib/features/explore/presentation/screens/explore_screen.dart
+++ lib/features/explore/presentation/screens/explore_screen.dart
@@ -1,11 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:gap/gap.dart';
-import 'package:iconsax/iconsax.dart';
-import 'package:instagram_clone/core/helper/extensions.dart';
-import 'package:instagram_clone/core/utils/app_colors.dart';
-import 'package:instagram_clone/core/widgets/custom_text_form_field.dart';
-
 import '../widgets/search_for_user_widget.dart';
 
 class ExploreScreen extends StatelessWidget {
