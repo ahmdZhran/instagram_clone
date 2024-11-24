5e7ad37c2321708cabe590547a30e09ebf8de595
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-24 10:38:20 +0200

    "Added ExploreRepository dependency to ExploreCubit, implemented searchUsers method, and updated ExploreState classes"

--- lib/features/explore/presentation/cubit/explore_cubit.dart
+++ lib/features/explore/presentation/cubit/explore_cubit.dart
@@ -1,8 +1,26 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instagram_clone/features/explore/data/explore_repository.dart';
 
 part 'explore_state.dart';
 
 class ExploreCubit extends Cubit<ExploreState> {
-  ExploreCubit() : super(ExploreInitial());
+  ExploreCubit(this._exploreRepository) : super(ExploreInitial());
+
+  final ExploreRepository _exploreRepository;
+
+  Future<void> searchUsers(String username) async {
+    //TOdO delete this after you display method
+    if (username.isEmpty) {
+      emit(ExploreInitial());
+      return;
+    }
+    emit(SearchUserLoading());
+    try {
+      final users = await _exploreRepository.searchUsers(username);
+      emit(SearchUserSuccess(users.docs));
+    } catch (error) {
+      emit(SearchUserFailure(errMessage: error.toString()));
+    }
+  }
 }
--- lib/features/explore/presentation/cubit/explore_state.dart
+++ lib/features/explore/presentation/cubit/explore_state.dart
@@ -6,7 +6,12 @@ final class ExploreInitial extends ExploreState {}
 
 final class SearchUserLoading extends ExploreState {}
 
-final class SearchUserSuccess extends ExploreState {}
+final class SearchUserSuccess extends ExploreState {
+  
+  final List<QueryDocumentSnapshot<Map<String, dynamic>>> users;
+
+  SearchUserSuccess(this.users);
+}
 
 final class SearchUserFailure extends ExploreState {
   final String errMessage;
