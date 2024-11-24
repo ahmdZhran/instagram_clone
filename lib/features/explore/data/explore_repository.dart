7f4d972d5eca7f52082357ea6a178b0ba3ee5c6a
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-24 11:32:55 +0200

    "Updated ExploreRepository to return List<UserEntity> instead of QuerySnapshot, added error handling and mapping of Firestore data to UserEntity"

--- lib/features/explore/data/explore_repository.dart
+++ lib/features/explore/data/explore_repository.dart
@@ -1,10 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagram_clone/features/explore/domain/entities/user_entity.dart';
 
 class ExploreRepository {
-  Future<QuerySnapshot<Map<String, dynamic>>> searchUsers(String username) {
-    return FirebaseFirestore.instance
-        .collection("users")
-        .where("user_name", isEqualTo: username)
-        .get();
+  Future<List<UserEntity>> searchUsers(String username) async {
+    try {
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection("users")
+          .where("user_name", isEqualTo: username)
+          .get();
+
+      return querySnapshot.docs
+          .map((doc) => UserEntity.fromFirestore(doc.data()))
+          .toList();
+    } catch (error) {
+      throw Exception("Failed to search users :$error");
+    }
   }
 }
