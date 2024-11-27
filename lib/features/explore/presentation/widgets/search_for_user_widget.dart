d0418beb311e9fede5cfaeb4537c524c364afeb0
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-27 12:59:19 +0200

    Remove import of 'flutter_screenutil/flutter_screenutil.dart' from SearchForUserWidget.

--- lib/features/explore/presentation/widgets/search_for_user_widget.dart
+++ lib/features/explore/presentation/widgets/search_for_user_widget.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:instagram_clone/core/router/routes.dart';
 import 'package:instagram_clone/core/utils/app_strings.dart';
