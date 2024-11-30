4137041c5fcea4234a80934525cf7c69c881263d
Ahmed <ahmedmohameddx80@gmail.com>
2024-11-30 13:29:48 +0200

    add TODO comment

--- lib/features/posts/domain/repositories/post_repository.dart
+++ lib/features/posts/domain/repositories/post_repository.dart
@@ -1,6 +1,8 @@
 import 'package:instagram_clone/features/posts/domain/entities/post_entity.dart';
 
 abstract class PostRepository {
+
+  //TODO if you there aren't any mehtod more delete this interface and just make the repo impl only and delete inhertance
   Future<void> createPost(PostEntity post);
   Future<void> deletePost(String postId);
 }
