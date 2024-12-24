ff3b87dd99ee2e6f1b593dd610793f4e234cd5f0
Ahmed <ahmedmohameddx80@gmail.com>
2024-12-24 20:55:24 +0200

    `Update dependencies and add Firebase Messaging plugin`

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'core/router/app_router.dart';
 import 'my_bloc_observer.dart';
 import 'app/instagram_app.dart';
