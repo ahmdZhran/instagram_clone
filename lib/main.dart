e092700983413eeae7a653615c35ee12e2e56173
Ahmed <ahmedmohameddx80@gmail.com>
2024-12-24 20:56:20 +0200

    `Added flutter_bloc package and import to main.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/router/app_router.dart';
 import 'my_bloc_observer.dart';
 import 'app/instagram_app.dart';
