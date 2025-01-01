01081ac59d1dea3e214f6c978908956b28d7d74d
Ahmed <ahmedmohameddx80@gmail.com>
2025-01-02 01:06:21 +0200

    `Refactor main function to use TokenDeviceManager instead of getAndSaveDeviceToken`

---
+++ lib/core/services/token_device_manager.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/core/helper/secure_storage_helper.dart';
+
+import '../../features/fcm_notification/fcm_services.dart';
+
+class TokenDeviceManager {
+  static final TokenDeviceManager _instance = TokenDeviceManager._internal();
+
+  TokenDeviceManager._internal();
+
+  factory TokenDeviceManager() => _instance;
+
+  final SecureStorageHelper _secureStorage = SecureStorageHelper();
+
+  Future<void> initializeToken() async {
+    try {
+      String? savedToken = await _secureStorage.read('device_token');
+
+      if (savedToken == null) {
+        String? deviceToken = await FCMService.getDeviceToken();
+        if (deviceToken != null) {
+          await _secureStorage.save('device_token', deviceToken);
+          debugPrint("FCM Token saved successfully: $deviceToken");
+        } else {
+          debugPrint("Failed to retrieve FCM Token.");
+        }
+      } else {
+        debugPrint("FCM Token already saved: $savedToken");
+      }
+    } catch (e) {
+      debugPrint("Error retrieving FCM Token: $e");
+    }
+  }
+
+  Future<String?> getToken() async {
+    return await _secureStorage.read('device_token');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'core/helper/secure_storage_helper.dart';
+import 'package:instagram_clone/core/services/token_device_manager.dart';
 import 'core/router/app_router.dart';
-import 'features/fcm_notification/fcm_services.dart';
 import 'my_bloc_observer.dart';
 import 'app/instagram_app.dart';
 import 'core/helper/shared_pref_helper.dart';
@@ -19,7 +18,7 @@ Future<void> main() async {
   Bloc.observer = MyBlocObserver();
   await Future.wait([
     InjectionContainer().init(),
-    getAndSaveDeviceToken(),
+    TokenDeviceManager().initializeToken(),
   ]);
 
   runApp(
@@ -29,23 +28,3 @@ Future<void> main() async {
   );
 }
 
-Future<void> getAndSaveDeviceToken() async {
-  try {
-    final secureStorage = SecureStorageHelper();
-    String? savedToken = await secureStorage.read('device_token');
-
-    if (savedToken == null) {
-      String? deviceToken = await FCMService.getDeviceToken();
-      if (deviceToken != null) {
-        await secureStorage.save('device_token', deviceToken);
-        debugPrint("FCM Token saved successfully: $deviceToken");
-      } else {
-        debugPrint("Failed to retrieve FCM Token.");
-      }
-    } else {
-      debugPrint("FCM Token already saved: $savedToken");
-    }
-  } catch (e) {
-    debugPrint("Error retrieving FCM Token: $e");
-  }
-}
