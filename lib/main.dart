4f1dad2ee30032603b3593207d15e81fdacb64c0
Ahmed <ahmedmohameddx80@gmail.com>
2025-01-02 00:58:32 +0200

    make it in main to make it just save it and save token once user open the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/helper/secure_storage_helper.dart';
 import 'core/router/app_router.dart';
+import 'features/fcm_notification/fcm_services.dart';
 import 'my_bloc_observer.dart';
 import 'app/instagram_app.dart';
 import 'core/helper/shared_pref_helper.dart';
@@ -17,6 +19,7 @@ Future<void> main() async {
   Bloc.observer = MyBlocObserver();
   await Future.wait([
     InjectionContainer().init(),
+    getAndSaveDeviceToken(),
   ]);
 
   runApp(
@@ -24,4 +27,25 @@ Future<void> main() async {
       appRouter: AppRouter(),
     ),
   );
+}
+
+Future<void> getAndSaveDeviceToken() async {
+  try {
+    final secureStorage = SecureStorageHelper();
+    String? savedToken = await secureStorage.read('device_token');
+
+    if (savedToken == null) {
+      String? deviceToken = await FCMService.getDeviceToken();
+      if (deviceToken != null) {
+        await secureStorage.save('device_token', deviceToken);
+        debugPrint("FCM Token saved successfully: $deviceToken");
+      } else {
+        debugPrint("Failed to retrieve FCM Token.");
+      }
+    } else {
+      debugPrint("FCM Token already saved: $savedToken");
+    }
+  } catch (e) {
+    debugPrint("Error retrieving FCM Token: $e");
+  }
 }
