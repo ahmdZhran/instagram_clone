327e752988c0f4a1eaa3ac6b8b517e704d403354
Ahmed <ahmedmohameddx80@gmail.com>
2025-01-09 22:13:05 +0200

    create auth manager class to help me call uid instead I use FirebaseAuth.instance.current.uid every time I need it

---
+++ lib/core/helper/auth_manager.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthManager {
+  static final AuthManager _instance = AuthManager._internal();
+  factory AuthManager() => _instance;
+  AuthManager._internal();
+
+  String? userId;
+
+  void initialize() {
+    userId = FirebaseAuth.instance.currentUser!.uid;
+  }
+}
