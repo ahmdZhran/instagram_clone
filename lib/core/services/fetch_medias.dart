ae6954db77393548a4b4f841eeb4984716b8b846
Ahmed <ahmedmohameddx80@gmail.com>
2025-07-10 05:44:34 +0300

    fix issue and run app without any problem

--- lib/core/services/fetch_medias.dart
+++ lib/core/services/fetch_medias.dart
@@ -3,6 +3,9 @@ import 'package:instagram_clone/features/posts/data/models/media_model.dart';
 import 'package:photo_manager/photo_manager.dart';
 import 'package:photo_manager_image_provider/photo_manager_image_provider.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:video_player/video_player.dart';
+
+import 'package:video_player/video_player.dart';
 
 Future<List<MediaModel>> fetchMedias({
   required AssetPathEntity album,
@@ -14,11 +17,20 @@ Future<List<MediaModel>> fetchMedias({
   try {
     final List<AssetEntity> entities =
         await album.getAssetListPaged(page: page, size: 30);
+
     final filteredEntities = entities.where((e) => e.type == type).toList();
+
     for (AssetEntity entity in filteredEntities) {
-      MediaModel media = MediaModel(
-        assetEntity: entity,
-        widget: FadeInImage(
+      Widget mediaWidget;
+
+      if (entity.type == AssetType.video) {
+        final file = await entity.file;
+        final controller = VideoPlayerController.file(file!);
+        await controller.initialize();
+
+        mediaWidget = VideoPlayer(controller);
+      } else {
+        mediaWidget = FadeInImage(
           placeholder: MemoryImage(kTransparentImage),
           fit: BoxFit.cover,
           image: AssetEntityImageProvider(
@@ -26,10 +38,13 @@ Future<List<MediaModel>> fetchMedias({
             thumbnailSize: const ThumbnailSize.square(500),
             isOriginal: false,
           ),
-        ),
-      );
+        );
+      }
 
-      medias.add(media);
+      medias.add(MediaModel(
+        assetEntity: entity,
+        widget: mediaWidget,
+      ));
     }
   } catch (e) {
     debugPrint('Error fetching media: $e');
